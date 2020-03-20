b0cba7e4c2563ef9a6391719415f2a37d3626789
John Ko <jko121790@gmail.com>
2020-03-20 15:29:43 -0700

    view average grade complete

--- components/page-header.js
+++ components/page-header.js
@@ -4,6 +4,8 @@ class PageHeader{
   }
 
   updateAverage(newAverage) {
+    var averageGradeElement = this.headerElement.querySelector("span");
+    averageGradeElement.textContent = newAverage;
     console.log(newAverage);
   }
 }
