385df51e944268b13020dd5901ed39b71ad22363
John Ko <jko121790@gmail.com>
2020-03-20 13:20:54 -0700

    requesting grade data

---
+++ components/app.js
@@ -0,0 +1,31 @@
+class App {
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+
+  handleGetGradesSuccess(grades) {
+
+    console.log(grades);
+  }
+
+
+
+  getGrades() {
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: {"X-Access-Token": "Tjll366U"},
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
+    })
+  }
+
+  start() {
+    console.log("start");
+    this.getGrades();
+  }
+}
