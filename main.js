8e26bf5427be8ec03512f4398501fb29e1052b23
John Ko <jko121790@gmail.com>
2020-03-21 20:04:49 -0700

    enhance GradeTable Component

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var tableElement = document.getElementById("table-student-grade");
 var headerElement = document.getElementById("header")
 var formElement = document.getElementById("grade-form");
+var noGradeElement = document.getElementById("noGradeElement");
 
 var pageHeader = new PageHeader(headerElement);
-var gradeTable = new GradeTable(tableElement);
+var gradeTable = new GradeTable(tableElement, noGradeElement);
 var gradeForm = new GradeForm(formElement);
 var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
